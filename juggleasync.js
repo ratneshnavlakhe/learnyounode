74e74745f0504c236926ede70910ad198b9e3236
ratneshnavlakhe <ratnesh061288@gmail.com>
2016-02-06 22:48:46 +0530

    Juggle sync done.

--- juggleasync.js
+++ juggleasync.js
@@ -1,8 +1,21 @@
 var http = require('http');
-var urlmod =  require('url');
-var urls = process.argv;
 
-urls.forEach(function (url) { 
+var urlmod =  require('url');
+
+//var urls = process.argv[2]; /url-2,url-3,url-4
+
+var urllist = [], strings = [];
+
+for(var i = 2; i < process.argv.length; i++) {
+	urllist[urllist.length] = process.argv[i];
+}
+
+//console.log(process.argv.length);
+
+//console.log(urllist);
+var i = 0;
+
+urllist.forEach(function (url) { 
 var string = '';
 	http.get(url, function (res) {
 		res.setEncoding('utf8');
@@ -12,8 +25,20 @@ var string = '';
 		});
 
 		res.on('end', function () {
-			console.log(string);
+
+			strings[i++] = string;
+
+			if(i === 3) {
+				//console.log(strings.length);
+				printdata(strings);
+			}
 		});
 	});
 });
+
+function printdata(data) {
+//console.log('here');
+	for(var j = 0; j < data.length; j++ ) console.log(data[j]);
+
+}
 
